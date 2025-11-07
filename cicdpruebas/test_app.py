3331271a968f7eb7c856c309ff31f177abd29edf
Alex <alexiscoello555@gmail.com>
2025-11-06 22:17:47 -0500

    Subiendo proyecto Flask con CI/CD

--- cicdpruebas/test_app.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_home():
-    cliente = app.test_client()
-    respuesta = cliente.get('/')
-    assert respuesta.status_code == 200
-    assert b"Hola Mundo desde Flask" in respuesta.data
-
